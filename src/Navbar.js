6b315b9832b998dca18b9ade9e83d98a7cfbeb32
martin <morales-martin@outlook.com>
2022-02-13 20:26:50 -0600

    navbar init

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavLink from './NavLink'
+
+function navbar() {
+  return (
+    <div>
+        <h1>Navbar</h1>
+        <NavLink></NavLink>
+    </div>
+  )
+}
+
+export default navbar
