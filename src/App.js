6c01ef3bf24109d4099c38e6adfc6343f80c7397
martin <morales-martin@outlook.com>
2022-02-13 21:21:08 -0600

    Navbar skeleton complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-import Navbar from './Navbar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import { Home } from './Components/Home';
+import { About } from './Components/About';
+import { Contact } from './Components/Contact';
+import { Gallery } from './Components/Gallery';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/gallery" exact component={Gallery} />
+        </Routes>
+      </Router>
+
+
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,53 @@
+import {React, useState} from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
+
+const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-logo" onClick={() => setOpen(false)}>
+        Logo
+      </Link>
+      <ul className={open ? "nav-links active" : "nav-links"}>
+        <li className="nav-item">
+          <Link to="/" className="nav-link" onClick={() => setOpen(false)}>
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link" onClick={() => setOpen(false)}>
+            About
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/contact"
+            className="nav-link"
+            onClick={() => setOpen(false)}
+          >
+            Contact
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/gallery"
+            className="nav-link"
+            onClick={() => setOpen(false)}
+          >
+            Gallery
+          </Link>
+        </li>
+      </ul>
+
+      <div onClick={() => setOpen(!open)} className="nav-icon">
+        {open ? <FiX /> : <FiMenu />}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
